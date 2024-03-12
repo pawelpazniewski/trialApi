4df7bbc5f675b46d2518c4f45d717a8235788e98
pawelpazniewski <62069745+pawelpazniewski@users.noreply.github.com>
2024-03-12 21:11:38 +0100

    Update server.mjs

--- src/server.mjs
+++ src/server.mjs
@@ -2,7 +2,7 @@ import express from 'express';
 
 const app = express();
 
-const PORT = process.env.PORT || 22;
+const PORT = process.env.PORT || 3000;
 
 app.get('/',(request,response)=>{
     response.status(200).send({
